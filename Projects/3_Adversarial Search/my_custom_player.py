4c873259ccf087cdf1063f2f3bf0b3d7ced0654e
AlfredoOrsini <orsinialfredo2@gmail.com>
2022-02-03 14:06:35 +0100

    Implemented Alfa Beta pruning

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -53,23 +53,27 @@ class CustomPlayer(DataPlayer):
 
     def minimax(self, state, depth):
 
-        def min_value(state, depth):
+        def min_value(state, a, b, depth):
             if state.terminal_test(): return state.utility(self.player_id)
             if depth <= 0: return self.score(state)
             value = float("inf")
-            for action in state.actions():
-                value = min(value, max_value(state.result(action), depth - 1))
+            for action in sorted(state.actions(), key=lambda x: self.score(state.result(x))):
+                value = min(value, max_value(state.result(action), a, b, depth - 1))
+                if value<=a: return value
+                b=min(b,value)
             return value
 
-        def max_value(state, depth):
+        def max_value(state, a, b, depth):
             if state.terminal_test(): return state.utility(self.player_id)
             if depth <= 0: return self.score(state)
             value = float("-inf")
-            for action in state.actions():
-                value = max(value, min_value(state.result(action), depth - 1))
+            for action in sorted(state.actions(), key=lambda x: self.score(state.result(x)), reverse=True):
+                value = max(value, min_value(state.result(action), a, b, depth - 1))
+                if value>=b: return value
+                a=max(a,value)
             return value
 
-        return max(state.actions(), key=lambda x: min_value(state.result(x), depth - 1))
+        return max(state.actions(), key=lambda x: min_value(state.result(x), float('-inf'), float('inf'), depth - 1))
 
     def score(self, state):
         own_loc = state.locs[self.player_id]
